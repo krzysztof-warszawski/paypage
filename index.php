3ce844d3006d89e88bddb5b68089f00d26ee2c03
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-08-25 13:38:34 +0200

    add init frontend

--- index.php
+++ index.php
@@ -9,6 +9,23 @@
     <title>Pay Page</title>
 </head>
 <body>
+    <form action="./charge.php" method="post" id="payment-form">
+        <div class="form-row">
+            <label for="card-element">
+                Credit or debit card
+            </label>
+            <div id="card-element">
+                <!-- A Stripe Element will be inserted here. -->
+            </div>
 
+            <!-- Used to display form errors. -->
+            <div id="card-errors" role="alert"></div>
+        </div>
+
+        <button>Submit Payment</button>
+    </form>
+
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="/js/charge.js"></script>
 </body>
 </html>
